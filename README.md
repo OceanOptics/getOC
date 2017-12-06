eea0476979a7ce945f4a15924eeb586d28eaf9e3
doizuc <nils.haentjens@maine.edu>
2017-12-05 19:47:30 -0500

    Add support for L1A data and OLCI

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ getOC
 
 _Bulk download Ocean Color images._
 
-This utility is made to easily bulk download Ocean Color images from [NASA Ocean Data](https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/) API. Provide a list of positions and dates in a csv file (ex: test.csv), select a sensor (MODIS_OC, MODIS_SST, VIIRS_OC), and the utility will get the images. Note that a strong internet connection is recommended.
+This utility is made to easily bulk download Ocean Color images from [NASA Ocean Data](https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/) API. Provide a list of positions and dates in a csv file (ex: test.csv), select an instrument (MODIS, VIIRS, OLCI), a processing level (L1A or L2), and a product (OC, IOP, or SST for L2 only) and the utility will get the images. Note that a strong internet connection is recommended.
 
-    python -Om getOC -s <sensor> <filename>
+    python -Om getOC -i <instrument> -l <level> -p <product> <filename>
 
 If you only need to download images in a given time frame without specifying a position, the [File Search](https://oceandata.sci.gsfc.nasa.gov/api/file_search) utility from NASA might be a better tool. The NASA utility also provides a few wget examples on this [Ocean Color Forum Post](https://oceancolor.gsfc.nasa.gov/forum/oceancolor/topic_show.pl?pid=12520).
 
--- getOC.py
+++ getOC.py
@@ -2,20 +2,29 @@
 """
 Bulk download Ocean Color images.
 
-  python -m getOC -s <sensor> <filename>
-  python getOC.py -s <sensor> <filename>
+  python -m getOC -i <instrument> -l <level> -p <product> <filename>
+  python getOC.py -i <instrument> -l <level> -p <product> <filename>
 
-Sensors supported are:
-    - VIIRS_OC
-    - MODIS_OC
-    - MODIS_SST (include night time images)
+instruments supported are:
+    - VIIRS
+    - MODIS
+    - OLCI
+
+level of processing supported are:
+    - L1A
+    - l2    [default]
+
+product supported are (only for level L2):
+    - OC    [default]
+    - IOP
+    - SST
 
 Path to a CSV file containing the following should be provided.
     - variable name: id,date&time,latitude,longitude
     - variable type/units: string,yyyy/mm/dd HH:MM:SS (UTC),degN,degE
 
-Only Ocean Color Level 2 is supported.
 Image within 24 hours are downloaded. (To verify)
+For OCLI only level L1 is supported (level and product arguments are ignored)
 
 author: Nils Haentjens
 created: Nov 28, 2017
@@ -51,17 +60,29 @@ import requests
 import re
 import os
 
-__version__ = "0.1"
+__version__ = "0.2"
 verbose = False
 
-# Set constants
+# Constants and query for file_search
 # LEVEL = {'All Types': 'all', 'Level 0': 'L0', 'Level 1': 'L1', 'Level 2': 'L2', 'Level 3 Bin': 'L3b', 'Level 3 SMI': 'L3m', 'Ancillary': 'MET', 'Miscellaneous': 'misc'}
 # MISSION = {'All Missions': 'all', 'Aquarius':'aquarius', 'SeaWiFS':'seawifs', 'MODIS Aqua':'aqua', 'MODIS Terra':'terra', 'MERIS':'meris', 'OCTS':'octs', 'CZCS':'czcs', 'HICO':'hico', 'VIIRS':'viirs'}
 # URL_FILE_SEARCH = 'https://oceandata.sci.gsfc.nasa.gov/api/file_search'
+# r = requests.post(URL_FILE_SEARCH, {'instrument':'octs', 'sdate':'1996-11-01', 'edate':'1997-01-01','dtype':'L2', 'addurl':1, 'results_as_file':1})
+
+# Set constants
 URL_BROWSE ='https://oceancolor.gsfc.nasa.gov/cgi/browse.pl'
 URL_GET_FILE = 'https://oceandata.sci.gsfc.nasa.gov/cgi/getfile/'
 
-def get_image_list(sensor, filename):
+# Documentation of Ocean Color Data Format Specification
+#   https://oceancolor.gsfc.nasa.gov/products/
+# Documentation regarding product definition
+#   https://oceancolor.gsfc.nasa.gov/products/
+INSTRUMENT_FILE_ID = {'SeaWiFS': 'S', 'MODIS-Aqua': 'A', 'TerraMODIS': 'T', 'OCTS': 'O','CZCS': 'C','MERIS': 'M','VIIRS': 'V','HICO': 'H', 'OLCI':'S3A_OL_1_ERR'}
+INSTRUMENT_QUERY_ID = {'SeaWiFS': 'MLAC', 'MODIS-Aqua': 'am', 'TerraMODIS': 'tm', 'OCTS': 'oc','CZCS': 'cz','MERIS': 'RR','VIIRS': 'v0','HICO': 'hi', 'OLCI':'ERR'};
+DATA_TYPE_ID = {'SeaWiFS':'LAC', 'MODIS-Aqua': 'LAC', 'TerraMODIS': 'LAC', 'OCTS': 'LAC', 'CZCS': '', 'MERIS':'RR', 'VIIRS': 'SNPP', 'HICO':'ISS', 'OLCI':'ERR'}
+EXTENSION_L1A = {'MODIS-Aqua': '', 'VIIRS': '.nc'};
+
+def get_image_list(filename, instrument, level='L2', product='OC'):
     # Get image name for each line of csv file provided
     #
     # CSV File should be formated as follow:
@@ -70,49 +91,91 @@ def get_image_list(sensor, filename):
 
     image_names = list()
     with open(filename) as fid:
-        # Pre-build query
-        if sensor == 'VIIRS_OC':
-            sen, sen_pre, sen_pos =  'v0', 'V', 'L2_SNPP_OC'# VIIRS (v0), MODIS Aqua (am)
-            dnm = 'D'
-        elif sensor == 'MODIS_OC':
-            sen, sen_pre, sen_pos =  'am', 'A', 'L2_LAC_OC'
-            dnm = 'D'
-        elif sensor == 'MODIS_SST':
-            sen, sen_pre, sen_pos =  'am', 'A', 'L2_LAC_SST'
-            dnm = 'D@N'
+        # Get parameters to build query
+        if instrument in INSTRUMENT_FILE_ID.keys():
+            if instrument == 'OLCI':
+                sen = '&typ=' + INSTRUMENT_QUERY_ID[instrument]
+                sen_pre = INSTRUMENT_FILE_ID[instrument]
+                sen_pos = '.zip'
+                dnm = 'D'
+                prm = 'TC'
+                sub = 'level1or2list'
+            else:
+                sen = '&sen=' + INSTRUMENT_QUERY_ID[instrument]
+                sen_pre = INSTRUMENT_FILE_ID[instrument]
+                if level == 'L2':
+                    # Level 2, need to specify product, adjust day|night
+                    sen_pos = level + '_' + DATA_TYPE_ID[instrument] + '_' + product + '.nc'
+                    if product in ['OC', 'IOP']:
+                        dnm = 'D'
+                        prm = 'CHL'
+                    elif product in ['SST']:
+                        dnm = 'D@N'
+                        prm = 'SST'
+                    else:
+                        if verbose:
+                            print('product not supported.')
+                        return None
+                    sub = 'level1or2list'
+                elif level in  ['L0', 'L1A']:
+                    # Level 1A specify daily data only
+                    sen_pos = level + '_' + DATA_TYPE_ID[instrument] + EXTENSION_L1A[instrument]
+                    dnm = 'D'
+                    prm = 'TC'
+                    sub = 'level1or2list'
+                # elif level == 'L3':
+                    # sub = 'level3'
+                else:
+                    if verbose:
+                        print('level not supported.')
+                    return None
         else:
-            print('Sensor not supported.')
-            sys.exit(-1)
-        # dnm = 'D' # day (D), night (N) or day and night (D@N)
-        sub = 'level1or2list'
+            if verbose:
+                print('instrument not supported.')
+            return None
+
         # For each line in csv file
         for l in csv.reader(fid, delimiter=','):
             lid, dt, lat, lon = l[0], datetime.strptime(l[1], '%Y/%m/%d %H:%M:%S'), float(l[2]), float(l[3])
             if verbose:
                 print('Querying ' + lid + ' ' + str(dt) + ' ' + str(lat) + ' ' + str(lon))
-            # Query file search (no location option)
-            # r = requests.post(URL_FILE_SEARCH, {'sensor':'octs', 'sdate':'1996-11-01', 'edate':'1997-01-01','dtype':'L2', 'addurl':1, 'results_as_file':1})
             # Build Query
             # Add some room in the given location (need to make it stronger if >180 | <-180)
             n, s = str(lat+1), str(lat-1)
             w, e = str(lon-2), str(lon+2)
             day = str((dt - datetime(1970,1,1)).days)
-            query = URL_BROWSE + '?sub=' + sub + '&sen=' + sen + '&per=DAY&day=' + day + '&n=' + n + '&s=' + s + '&w=' + w + '&e=' + e + '&dnm=' + dnm
+            query = URL_BROWSE + '?sub=' + sub + sen + '&per=DAY&day=' + day + '&n=' + n + '&s=' + s + '&w=' + w + '&e=' + e + '&dnm=' + dnm + '&prm=' + prm
             # Query API
             r = requests.get(query)
-            # Parse html
-            regex = re.compile('filenamelist&id=(\d+\.\d+)')
-            filenamelist_id = regex.findall(r.text)
-            if not filenamelist_id:
-                # Case one image
-                regex = re.compile('' + sen_pre + '\d+\.'+ sen_pos + '.nc')
-                image_names.extend(list(set(regex.findall(r.text)))) # Get unique id
+            if instrument == 'OLCI':
+                # Parse html
+                regex = re.compile(sen_pre + '(.*?)'+ sen_pos)
+                image_names.extend(list(set(regex.findall(r.text))))
             else:
-                # Case multiple images
-                r = requests.get(URL_BROWSE + '?sub=filenamelist&id=' + filenamelist_id[0])
-                for foo in r.text.splitlines():
-                    image_names.append(foo)
+                # Parse html
+                regex = re.compile('filenamelist&id=(\d+\.\d+)')
+                filenamelist_id = regex.findall(r.text)
+                if not filenamelist_id:
+                    # Case one image
+                    regex = re.compile(sen_pre + '\d+\.'+ sen_pos)
+                    image_names.extend(list(set(regex.findall(r.text)))) # Get unique id
+                else:
+                    # Case multiple images
+                    r = requests.get(URL_BROWSE + '?sub=filenamelist&id=' + filenamelist_id[0] + '&prm=' + prm)
+                    for foo in r.text.splitlines():
+                        image_names.append(foo)
+
+        # Reformat list
+        if level == 'L2' and product == 'IOP':
+            image_names = [image_name.replace('OC', 'IOP') for image_name in image_names]
+        if instrument == 'MODIS-Aqua' and level == 'L1A':
+            image_names = [image_name + '.bz2' for image_name in image_names]
+        if instrument == 'OLCI':
+            image_names = [sen_pre + image_name + sen_pos for image_name in image_names]
         return list(set(image_names))
+    if verbose:
+        print('unable to open file.')
+    return None
 
 def download(image_names):
     # Download all images provided in list
@@ -139,14 +202,16 @@ if __name__ == "__main__":
     from optparse import OptionParser
 
     parser = OptionParser(usage="Usage: getOC.py [options] filename", version="getOC " + __version__)
-    parser.add_option("-s", "--sensor", action="store", dest="sensor", help="specify sensor, available options are: VIIRS_OC, MODIS_OC, and MODIS_SST")
+    parser.add_option("-i", "--instrument", action="store", dest="instrument", help="specify instrument, available options are: VIIRS, MODIS-Aqua, and OLCI")
+    parser.add_option("-l", "--level", action="store", dest="level", default='L2', help="specify processing level, available options are: L1A, and L2")
+    parser.add_option("-p", "--product", action="store", dest="product", default='OC', help="specify product identifier, available options are: OC, SST, and IOP")
     parser.add_option("-q", "--quiet", action="store_false", dest="verbose", default=True)
     (options, args) = parser.parse_args()
 
     verbose = options.verbose
-    if options.sensor is None:
+    if options.instrument is None:
         print(parser.usage)
-        print('getOC.py: error: option -s, --sensor is required')
+        print('getOC.py: error: option -i, --instrument is required')
         sys.exit(-1)
 
     if len(args) < 1:
@@ -158,5 +223,6 @@ if __name__ == "__main__":
         print('getOC.py: error: too many arguments')
         sys.exit(-1)
 
-    image_names = get_image_list(options.sensor, args[0])
-    download(image_names)
+    image_names = get_image_list(args[0], options.instrument, options.level, options.product)
+    if image_names is not None:
+        download(image_names)
